db8f244eeb1e2303dedd112634c59a467839e5a6
avokado0h <simon.ruhnau@protonmail.com>
2020-04-02 23:33:15 +0200

    added fps on example

--- PygamePlotExample.py
+++ PygamePlotExample.py
@@ -9,7 +9,7 @@ from pygameplotlib.PygamePlot import Plot as plt
 from pygameplotlib.PygameUtils import Settings
 from pygameplotlib.PygameUtils import showText
 
-a = Settings(width=1000,height=400)
+a = Settings(width=1030,height=400)
 
 f = 40.0
 T = 1/f
@@ -18,11 +18,10 @@ Ts = 1/fs
 
 t = np.arange(0,30*T,Ts)
 
-# create 4 plot objects
-signal = plt(a,500,350,xlabel='time [s]',ylabel='y(t)',
-        xstep=5,caption='Signal',xlim=[0,30*T])
+signal = plt(a,500,350,xlabel='t [s]',ylabel='y(t)',
+        xstep=5,caption='Signal',xlim=[0,5*T])
 spectrum = plt(a,500,350,xlabel='frequency [Hz]',ylabel='|Y|',
-        xstep=5,xlim=[30,70],ylim=[0,600],caption='Spectrum')
+        xstep=5,xlim=[30,70],ylim=[0,300],caption='Spectrum')
 window = plt(a,350,350)
 
 hamming = np.hanning(len(t))
@@ -40,19 +39,18 @@ d = 1
 running = True
 while running:
 
-    if f <= 40:
-        d = 1
-    if f >= 60:
-        d = -1 
+    if f <= 40: d = 1
+    if f >= 60: d = -1
     f += d*0.25 
 
-    y = np.sin(2*np.pi*50*t) + np.sin(2*np.pi*f*t)
+    y = np.sin(2*np.pi*f*t)
     fspec,Y = getSpectrum(y)
 
     signal.Show(10,10,t,y,0,'green')
     spectrum.Show(520,10,fspec,Y,0,'blue')
 
-    signalTxt = 'y(t) = sin(2*pi*50Hz*t) + 2*sin(2*pi*{0:.2f}*t)'.format(f)
-    showText(a,150,365,signalTxt,30)
+    signalTxt = 'y(t) = sin(2*pi*{0:.2f}*t)'.format(f)
+    showText(a,150,365,signalTxt,20)
+    showText(a,925,370,'FPS: {0:.2f}'.format(a.fps),15)
 
     running = a.Update()
